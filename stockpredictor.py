ffe7f9fcc1bed83956a9c3bfb7e2735d15f1bb8e
omhela25-stack <yashhela25@gmail.com>
2025-08-28 20:23:17 +0530

    Update stockpredictor.py

--- stockpredictor.py
+++ stockpredictor.py
@@ -17,7 +17,7 @@ warnings.filterwarnings('ignore')
 ALPHA_VANTAGE_API_KEY = "LPRQX827JWWLKA4R"   # replace with your key
 AV_BASE_URL = "https://www.alphavantage.co/query"
 
-st.set_page_config(page_title="📈 SWATI ", layout="wide")
+
 st.title("MY LOVE SWATI")
 
 
@@ -183,6 +183,7 @@ if predict_btn:
             # Data Table
             st.subheader("📋 Recent Data")
             st.dataframe(df.tail(20))
+
 
 
 
